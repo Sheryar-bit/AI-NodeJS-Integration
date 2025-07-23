ec4ab5873998a1ef80cf46d8b202dc54f26f652e
Sheryar <alisharyar93@gmail.com>
2025-07-23 17:47:11 +0500

    ` Basics of transformers (sentiment-analysis) `

--- Python/transformer.py
+++ Python/transformer.py
@@ -1,9 +1,14 @@
 from transformers import pipeline
 
-classifier = pipeline("sentiment-analysis")
-print(classifier("I love using transformers!"))
+classifier = pipeline("sentiment-analysis")   # Load the sentiment analysis model
+user_input = input("Enter a sentence to analyze sentiment ")
+result = classifier(user_input)# Analyze the sentiment
 
-#OutPut:
-#[{'label': 'postive', 'score': 0.99..}]
+print(f"Sentiment: {result[0]['label']}, Confidence: {result[0]['score']:.2f}")
+
+##result:
+# Enter a sentence to analyze sentiment
+# hello i am sick today feel too low
+# Sentiment: {NEGATIVE, Confidence: 1.00}
 
 #Not ideal as requires bhot zyada storage; will start with hugging face ssoon.
